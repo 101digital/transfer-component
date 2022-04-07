31b5b0ef0e82b464675346c3a6c946a1917505c8
Tuyen Nguyen <tuyennguyen@Tuyen-Nguyens-Macbook.local>
2022-04-07 16:08:12 +0700

    update

--- index.d.ts
+++ index.d.ts
@@ -5,3 +5,5 @@ export * from './src/type';
 export { default as TransferComponent } from './src/components/transfer-component';
 export { default as SelectUDAccountComponent } from './src/components/select-ud-account-component';
 export { default as NewRecipientComponent } from './src/components/new-recipient-component';
+export { default as SelectEBankComponent } from './src/components/select-ebank-component';
+export { default as TransferMethodComponent } from './src/components/transfer-method-component';
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 export { TransferService } from './src/service/transfer-service';
 export { TransferContext } from './src/context/transfer-context';
 export { default as TransferProvider } from './src/context/transfer-provider';
-export * from './src/type'
+export * from './src/type';
 export { default as TransferComponent } from './src/components/transfer-component';
 export { default as SelectUDAccountComponent } from './src/components/select-ud-account-component';
 export { default as NewRecipientComponent } from './src/components/new-recipient-component';
+export { default as SelectEBankComponent } from './src/components/select-ebank-component';
+export { default as TransferMethodComponent } from './src/components/transfer-method-component';
